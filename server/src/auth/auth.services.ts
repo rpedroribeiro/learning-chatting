60ef301a1c3c936b190945924b2aaabcbe73bf80
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-06-23 11:44:21 -0700

    changed again how the file is exported, function stays the same

--- server/src/auth/auth.services.ts
+++ server/src/auth/auth.services.ts
@@ -3,51 +3,69 @@ import { UUID } from 'crypto'
 import db from '../database/prisma'
 import authJwt from './auth.jwt'
 
-const authServices = () => {
-  /**
-   * This funciton takes in the user email and uses
-   * prisma's ORM to return a user object.
-   * 
-   * @param email - The email of the user.
-   * @returns - The user object from the db.
-   */
-  const findUserByEmail = async (email: string) => {
-    return await db.user.findUnique({
-      where:{
-        email,
-      }
-    })
-  }
+/**
+ * This funciton takes in the user email and uses
+ * prisma's ORM to return a user object.
+ * 
+ * @param email - The email of the user.
+ * @returns - The user object from the db.
+ */
+const findUserByEmail = async (email: string) => {
+  return await db.user.findUnique({
+    where:{
+      email,
+    }
+  })
+}
 
-  /**
-   * This funciton takes in the user id and uses
-   * prisma's ORM to return a user object.
-   * 
-   * @param id - The id of the user.
-   * @returns - The user object from the db.
-   */
-  const findUserById = async (id: UUID) => {
-    return await db.user.findUnique({
-      where:{
-        id,
-      }
-    })
-  }
+/**
+ * This funciton takes in the user id and uses
+ * prisma's ORM to return a user object.
+ * 
+ * @param id - The id of the user.
+ * @returns - The user object from the db.
+ */
+const findUserById = async (id: UUID) => {
+  return await db.user.findUnique({
+    where:{
+      id,
+    }
+  })
+}
 
-  /**
-   * This function creates a user with the default auth of
-   * email and password and returns the new user object.
-   * 
-   * @param user - All the user information to create an
-   * account
-   * @returns - The user object from prisma's ORM
-   */
-  const createUserByEmailAndPassword = async (user: any) => {
-    user.password = bcrypt.hashSync(user.password, 12)
-    return db.user.create({
-      data: user,
-    })
-  }
+/**
+ * This function creates a user with the default auth of
+ * email and password and returns the new user object.
+ * 
+ * @param user - All the user information to create an
+ * account
+ * @returns - The user object from prisma's ORM
+ */
+const createUserByEmailAndPassword = async (user: any) => {
+  user.password = bcrypt.hashSync(user.password, 12)
+  return db.user.create({
+    data: user,
+  })
+}
+
+/**
+ * 
+ * @param refreshToken 
+ * @param userId 
+ * @returns 
+ */
+const addRefreshTokenToWhiteList = ( refreshToken: string, userId: UUID) => {
+  return db.refreshToken.create({
+    data: {
+      hashedToken: authJwt.hashToken(refreshToken),
+      userId,
+      expireAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+    },
+  })
+}
+
+const authServices = {
+
 }
 
 export default authServices

c26f4af69afb900382447af0bea1f0b61ac01a82
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-07 12:34:50 -0700

    encased previours routes in a try and catch statement and made new route to handle submission status update

--- server/src/assignments/assignments.routes.ts
+++ server/src/assignments/assignments.routes.ts
@@ -21,32 +21,36 @@ const upload = multer({ dest: 'uploads/assignments'})
  * information provided.
  */
 router.post('/:userId/class/:classId/assignment', upload.array('uploadedFiles'), authenticateToken, async (req, res, next) => {
-  const { assignmentName, assignmentDescription, dueDate } = req.body
-  const classId = req.params.classId
-  const userId = req.params.userId
+  try {
+    const { assignmentName, assignmentDescription, dueDate } = req.body
+    const classId = req.params.classId
+    const userId = req.params.userId
 
-  const currClass = await classService.findProfessorByClassId(classId, ctx)
-  if (currClass?.professor.id !== userId) {
-    res.status(400).json({message: "Only the professor of this course can create an assignment"})
-    throw new Error('Only the professor of this course can create an assignment')
+    const currClass = await classService.findProfessorByClassId(classId, ctx)
+    if (currClass?.professor.id !== userId) {
+      res.status(400).json({message: "Only the professor of this course can create an assignment"})
+      throw new Error('Only the professor of this course can create an assignment')
+    }
+
+    let filesUrls
+    const files = req.files as Express.Multer.File[]
+    if (files !== undefined && files.length > 0) {
+      filesUrls = await assignmentUtils.uploadAssignmentFiles(files, assignmentName)
+    }
+
+    const newAssignment = await assignmentServices.createNewAssignment(
+      assignmentName,
+      assignmentDescription,
+      classId,
+      dueDate,
+      filesUrls || undefined,
+      ctx
+    )
+
+    res.status(200).json({assignment: newAssignment})
+  } catch (error) {
+    console.error(error)
   }
-
-  let filesUrls
-  const files = req.files as Express.Multer.File[]
-  if (files !== undefined && files.length > 0) {
-    filesUrls = await assignmentUtils.uploadAssignmentFiles(files, assignmentName)
-  }
-
-  const newAssignment = await assignmentServices.createNewAssignment(
-    assignmentName,
-    assignmentDescription,
-    classId,
-    dueDate,
-    filesUrls || undefined,
-    ctx
-  )
-
-  res.status(200).json({assignment: newAssignment})
 })
 
 /**
@@ -56,41 +60,45 @@ router.post('/:userId/class/:classId/assignment', upload.array('uploadedFiles'),
  * made once the submission is submitted.
  */
 router.post('/:userId/class/:classId/assignment/:assignmentId/uploadfiles', upload.single('uploadedFiles'), authenticateToken, async (req, res, next) => {
-  const userId = req.params.userId
-  const assignmentId = req.params.assignmentId
+  try {
+    const userId = req.params.userId
+    const assignmentId = req.params.assignmentId
 
-  const currSubmission = await submissionServices.findSubmissionWithUserIdAndAssignmentId(
-    userId,
-    assignmentId,
-    ctx
-  )
+    const currSubmission = await submissionServices.findSubmissionWithUserIdAndAssignmentId(
+      userId,
+      assignmentId,
+      ctx
+    )
 
-  if (currSubmission?.submitted === true) {
-    res.status(400).json({message: "Cannot make this request when assignment is already submitted"})
-    throw new Error('Cannot make this request when assignment is already submitted')
+    if (currSubmission?.submitted === true) {
+      res.status(400).json({message: "Cannot make this request when assignment is already submitted"})
+      throw new Error('Cannot make this request when assignment is already submitted')
+    }
+
+    const currUser = await authServices.findUserById(userId, ctx)
+    if (!currUser || currUser.accountType !== UserRole.Student) {
+      res.status(400).json({message: "User is not a student or does not exist"})
+      throw new Error('User is not a student or does not exist')
+    }
+
+    const file = req.file
+    if (!file) {
+      res.status(400).json({message: "No file sent on request"})
+      throw new Error('No file sent on request')
+    }
+
+    const filePath = file.path
+
+    const submission = await submissionServices.uploadSubmissionFile(
+      userId,
+      assignmentId,
+      filePath,
+      ctx
+    )
+    res.status(200).json({submission: submission})
+  } catch (error) {
+    console.error(error)
   }
-
-  const currUser = await authServices.findUserById(userId, ctx)
-  if (!currUser || currUser.accountType !== UserRole.Student) {
-    res.status(400).json({message: "User is not a student or does not exist"})
-    throw new Error('User is not a student or does not exist')
-  }
-
-  const file = req.file
-  if (!file) {
-    res.status(400).json({message: "No file sent on request"})
-    throw new Error('No file sent on request')
-  }
-
-  const filePath = file.path
-
-  const submission = await submissionServices.uploadSubmissionFile(
-    userId,
-    assignmentId,
-    filePath,
-    ctx
-  )
-  res.status(200).json({submission: submission})
 })
 
 /**
@@ -99,40 +107,81 @@ router.post('/:userId/class/:classId/assignment/:assignmentId/uploadfiles', uplo
  * be made once the submission is submitted.
  */
 router.delete('/:userId/class/:classId/assignment/:assignmentId/uploadfiles', authenticateToken, async (req, res, next) => {
-  const userId = req.params.userId
-  const assignmentId = req.params.assignmentId
-  const { filePath } = req.body
+  try {
+    const userId = req.params.userId
+    const assignmentId = req.params.assignmentId
+    const { filePath } = req.body
 
-  const currSubmission = await submissionServices.findSubmissionWithUserIdAndAssignmentId(
-    userId,
-    assignmentId,
-    ctx
-  )
+    const currSubmission = await submissionServices.findSubmissionWithUserIdAndAssignmentId(
+      userId,
+      assignmentId,
+      ctx
+    )
 
-  if (currSubmission?.submitted === true) {
-    res.status(400).json({message: "Cannot make this request when assignment is already submitted"})
-    throw new Error('Cannot make this request when assignment is already submitted')
+    if (currSubmission?.submitted === true) {
+      res.status(400).json({message: "Cannot make this request when assignment is already submitted"})
+      throw new Error('Cannot make this request when assignment is already submitted')
+    }
+
+    const currUser = await authServices.findUserById(userId, ctx)
+    if (!currUser || currUser.accountType !== UserRole.Student) {
+      res.status(400).json({message: "User is not a student or does not exist"})
+      throw new Error('User is not a student or does not exist')
+    }
+
+    await gcpBucketUtils.deleteObjectFromBucket(filePath)
+
+    const updatedSubmission = await submissionServices.deleteSubmissionFile(
+      userId,
+      assignmentId,
+      filePath,
+      ctx
+    )
+
+    if (updatedSubmission === null) {
+      res.status(400).json({message: "Issue removing the file from the submission"})
+      throw new Error('Issue removing the file from the submission')
+    }
+
+    res.status(200).json({submission: updatedSubmission})
+  } catch (error) {
+    console.error(error)
   }
+})
 
-  const currUser = await authServices.findUserById(userId, ctx)
-  if (!currUser || currUser.accountType !== UserRole.Student) {
-    res.status(400).json({message: "User is not a student or does not exist"})
-    throw new Error('User is not a student or does not exist')
+/**
+ * 
+ */
+router.put('/:userId/class/:classId/assignment/:assignmentId/submit', authenticateToken, async (req, res, next) => {
+  try {
+    const userId = req.params.userId
+    const assignmentId = req.params.assignmentId
+
+    const currSubmission = await submissionServices.findSubmissionWithUserIdAndAssignmentId(
+      userId,
+      assignmentId,
+      ctx
+    )
+
+    if (currSubmission?.submitted === true) {
+      res.status(400).json({message: "Cannot make this request when assignment is already submitted"})
+      throw new Error('Cannot make this request when assignment is already submitted')
+    }
+
+    const currUser = await authServices.findUserById(userId, ctx)
+    if (!currUser || currUser.accountType !== UserRole.Student) {
+      res.status(400).json({message: "User is not a student or does not exist"})
+      throw new Error('User is not a student or does not exist')
+    }
+
+    const updatedSubmission = await submissionServices.updateSubmissionStatus(
+      userId,
+      assignmentId,
+      ctx
+    )
+
+    res.status(200).json({submission: updatedSubmission})
+  } catch (error) {
+    console.error(error)
   }
-
-  await gcpBucketUtils.deleteObjectFromBucket(filePath)
-
-  const updatedSubmission = await submissionServices.deleteSubmissionFile(
-    userId,
-    assignmentId,
-    filePath,
-    ctx
-  )
-
-  if (updatedSubmission === null) {
-    res.status(400).json({message: "Issue removing the file from the submission"})
-    throw new Error('Issue removing the file from the submission')
-  }
-
-  res.status(200).json({submission: updatedSubmission})
 })

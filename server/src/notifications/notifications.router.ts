3b928d384774bb32280ffd58edc138b60bfcf44f
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-09 17:51:53 -0700

    added GET route to fetch all notifications and a PUT request to mark a notification as read

--- server/src/notifications/notifications.router.ts
+++ server/src/notifications/notifications.router.ts
@@ -9,6 +9,35 @@ import notificationServices from './notifications.services'
 const router = express.Router()
 const ctx = { prisma }
 
+/**
+ * This GET route fetches all notifications from a specific category provided
+ * for a user in a class, only the notifications that were made inside that class
+ * are returned.
+ */
+router.get('/:userId/class/:classId/notifications', authenticateToken, async (req, res, next) => {
+  try {
+    const { notificationType } = req.body
+    const userId = req.params.userId
+    const classId = req.params.classId
+
+    const notifications = await notificationServices.getAllNotificationsForCategoryForUser(
+      userId,
+      classId,
+      notificationType,
+      ctx
+    )
+
+    res.status(200).json({notifications: notifications})
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+/**
+ * This POST request creates a new notification and links it with the creator and
+ * all of the receivers, it is also given a category and meta data to go alongside
+ * the request.
+ */
 router.post('/:userId/class/:classId/notifications', authenticateToken, async (req, res, next) => {
   try {
     const { data, notificationType } = req.body
@@ -71,6 +100,23 @@ router.post('/:userId/class/:classId/notifications', authenticateToken, async (r
         ctx
       )
     }
+  } catch (error) {
+    console.error(error)
+  }
+})
+
+/**
+ * This PUT request updates a notification to be read using the notification id
+ * provided.
+ */
+router.put('/:userId/class/:classId/notifications/:notificationId', authenticateToken, async (req, res, next) => {
+  try {
+    const notificationId = req.params.notificationId
+    const updatedNotification = await notificationServices.markNotificationAsRead(
+      notificationId,
+      ctx
+    )
+    res.status(200).json({updatedNotification: updatedNotification})
   } catch (error) {
     console.error(error)
   }

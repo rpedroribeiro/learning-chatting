f746573dd8f23015e5c98b4fdf8eb70d58ae5be3
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-14 16:35:45 -0700

    pages handle notification popup

--- client/src/pages/ClassPage.tsx
+++ client/src/pages/ClassPage.tsx
@@ -1,17 +1,21 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "../components/Sidebar"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import useClassroom from "../hooks/useClassroom"
 import '../styles/course.css'
 import useAuth from "../hooks/useAuth"
+import notificationsUtils from "../utils/notificationsUtils"
+import classesApi from "../api/classesApi"
+import NotificationBanner from "../components/notifications/NotificationBanner"
+import '../styles/notifications.css'
 
 const ClassPage = () => {
+  const [notificationClassName, setNotificationClassName] = useState<string>('')
+  const [notificationType, setNotificationType] = useState<any>(null)
+  const [notificationMessage, setNotificationMessage] = useState<any>(null)
+  const [toggleBanner, setToggleBanner] = useState<boolean>(false)
   const { setIsClassroom } = useClassroom()
-  const { socket } = useAuth()
-
-  const handleNotificationReceived = (classId: any, notificationType: any, data: any) => {
-    
-  }
+  const { socket, userId } = useAuth()
 
   useEffect(() => {
     setIsClassroom(true)
@@ -19,6 +23,30 @@ const ClassPage = () => {
 
   useEffect(() => {
     if (!socket) return
+    const handleNotificationReceived = async (classId: any, notificationType: any, data: any) => {
+      console.log(data)
+      const formattedMessage = notificationsUtils.formatNotificationMessage(
+        data,
+        notificationType
+      )
+      console.log(formattedMessage)
+      const [classInfo, status, message] = await classesApi.getClassDetails(
+        userId,
+        classId
+      )
+      if (status) {
+        setNotificationClassName(classInfo.name)
+        setNotificationType(notificationType)
+        setNotificationMessage(formattedMessage)
+        setToggleBanner(true)
+        setTimeout(() => {
+          setNotificationClassName('')
+          setNotificationType(null)
+          setNotificationMessage(null)
+          setToggleBanner(false)
+        })
+      }
+    }
     socket.on('notificationPosted', handleNotificationReceived)
     return () => {
       socket?.off('notificationPosted', handleNotificationReceived)
@@ -27,6 +55,12 @@ const ClassPage = () => {
 
   return (
     <div className="course-page">
+      <NotificationBanner
+        className={`notification-banner ${toggleBanner ? 'visible' : ''}`} 
+        notificationClassName={notificationClassName}
+        notificationType={notificationType}
+        notificationMessage={notificationMessage}
+      />
       <Sidebar />
       <Outlet />
     </div>

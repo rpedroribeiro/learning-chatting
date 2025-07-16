1b9e13c0a25bef8f7ff5e7e0e45666f17344f8e1
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-15 18:04:07 -0700

    added chat input and img upload

--- client/src/components/chatting/ChattingContainer.tsx
+++ client/src/components/chatting/ChattingContainer.tsx
@@ -1,15 +1,34 @@
 import useClassroom from '../../hooks/useClassroom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faImage } from '@fortawesome/free-solid-svg-icons'
 import '../../styles/chatting.css'
+import { useState } from 'react'
 
 const ChattingContainer = () => {
+  const [chatInput, setChatInput] = useState<string>('')
+  const [file, setFile] = useState<null | File>(null)
   const { currClass } = useClassroom()
+
+  const handleFileChange = (event: any) => {
+    const file = event.target.files?.[0]
+    if (file) { setFile(file) }
+  }
+
   return (
     <div className="chatting-page">
       <div className='chatting-header'>
         <h1>{currClass.className} | Class Chat</h1>
       </div>
       <div className='chatbox-container'>
-        
+        <div className='input-container'>
+          <input value={chatInput} onChange={(event) => setChatInput(event.target.value)}/>
+          <div style={{display: 'flex'}}>
+            <label htmlFor="chat-file-upload" style={{ cursor: 'pointer' }}>
+              <FontAwesomeIcon icon={faImage} />
+            </label>
+            <input onChange={(event) => handleFileChange(event)} id='chat-file-upload' type='file' style={{ display: 'none' }}/>
+          </div>
+        </div>
       </div>
     </div>
   )

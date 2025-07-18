046652cfb68bac56cb594efcbd2c9bba60923e9c
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-17 17:30:29 -0700

    added getter and setter methods to private attributes, created method to trim token of suffixes

--- client/src/utils/commandBot.ts
+++ client/src/utils/commandBot.ts
@@ -3,23 +3,58 @@ class commandBot {
   private synonymMap: Map<string, Set<string>>
   private reverseSynonymMap: Map<string, string>
   private suffixes: string[]
-  private termFrequencies: Record<string, number>
-  private sentenceFrequency: Record<string, number>
+  private termFrequencies: Map<string, number>
   private sentenceCount: number
   private sentenceVectorMap: Map<string, (number | undefined)[]>
 
   constructor(targetSentences: string[]) {
     const synonyms = new Map<string, Set<string>>([
-      ["get", new Set(["find", "fetch"])]
+      ["get", new Set(["find", "fetch", "search"])],
+      ["post", new Set(["create", "make"])]
     ])
     this.vocabWords = new Set<string>()
     this.synonymMap = synonyms
     this.reverseSynonymMap = this.createReverseSynonymMap(synonyms)
-    this.suffixes = []
-    this.termFrequencies = {}
-    this.sentenceFrequency = {}
+    this.suffixes = ["ing", "ed", "es", "s"]
     this.sentenceCount = 0
+    this.termFrequencies = new Map<string, number>()
     this.sentenceVectorMap = this.computeTargetSentenceVectors(targetSentences)
+  }
+
+  /**
+   * This method returns the set of vocab words.
+   * 
+   * @returns The set of vocab words.
+   */
+  private getVocabWords(): Set<string> {
+    return this.vocabWords
+  }
+
+  /**
+   * This method sets the vocab words.
+   * 
+   * @param words - The words to be set as the vocab words.
+   */
+  private setVocabWords(words: Set<string>): void {
+    this.vocabWords = words
+  }
+
+  /**
+   * This method sets the word frequency map.
+   * 
+   * @param wordFrequency - The build word frequency map.
+   */
+  private setTermFrequency(wordFrequency: Map<string, number>): void {
+    this.termFrequencies = wordFrequency
+  }
+
+  /**
+   * This method stores the number of target sentences.
+   * 
+   * @param sentences - The target sentences.
+   */
+  private setSentenceCount(sentences: string[]): void {
+    this.sentenceCount = sentences.length
   }
 
   /**
@@ -29,16 +64,34 @@ class commandBot {
    * @param sentences - The target sentences we want to map.
    * @returns A map of the sentence as the key and the vector as the value.
    */
-  computeTargetSentenceVectors(sentences: string[]): Map<string, number[]> {
+  private computeTargetSentenceVectors(sentences: string[]): Map<string, number[]> {
+    const words = new Set<string>([])
+    const wordFrequency = new Map<string, number>([])
+    const allTokenizedSentence = new Set<string[]>()
+    this.setSentenceCount(sentences)
     for (const sentence of sentences) {
       const [tokenizedSentence, tokenizedParams] = this.tokenize(sentence)
+      allTokenizedSentence.add(tokenizedSentence)
+      for (const token in tokenizedSentence) {
+        if (wordFrequency.has(token)) { wordFrequency.set(token, wordFrequency.get(token)! + 1) }
+        else { wordFrequency.set(token, 1) }
+        words.add(token) 
+      }
     }
+    this.setTermFrequency(wordFrequency)
+    this.setVocabWords(words)
+    for (const tokenizedSentence of allTokenizedSentence) {
+      this.vectorize(tokenizedSentence)
+    }
+  }
 
-    return // TODO
+  // Step 8: Find the closest target sentence match to the input sentence
+  findClosestMatch(inputSentence: string): { sentence: string; similarity: number } {
+    
   }
 
   /**
-   * This meethod takes in a sentence and tokenizes the words and the params seperately.
+   * This method takes in a sentence and tokenizes the words and the params seperately.
    * 
    * @param sentence - The sentence to be tokenized.
    * @returns The tokens of words and params.
@@ -52,8 +105,9 @@ class commandBot {
       if (match[1] === undefined) {  params.push(match[1]) } 
       else if (match[0]) {
         const token = match[0]
-        const finalToken = this.normalizeToken(token.toLocaleLowerCase())
-        tokens.push(finalToken)
+        const normalizedToken = this.normalizeToken(token.toLocaleLowerCase())
+        const stemedToken = this.stemToken(normalizedToken)
+        tokens.push(stemedToken)
       }
     }
     return [tokens, params]
@@ -85,27 +139,30 @@ class commandBot {
    * @param token - The token we try to find the synonym of. 
    * @returns Either the synonym or the original token.
    */
-  normalizeToken(token: string): string {
+  private normalizeToken(token: string): string {
     const normalized = this.reverseSynonymMap.get(token)
     if (normalized !== undefined) { return normalized }
     return token
   }
 
-  // Step 3: Remove the suffixes
-  stemToken(token: string): string {
-  }
-
-  // Step 4: Extract parameter and count them
-  exctractParameters(tokens: string[]): { newSentence: string, paramCount: number } {
-
+  /**
+   * 
+   * @param token 
+   * @returns 
+   */
+  private stemToken(token: string): string {
+    const matchingSuffixes = this.suffixes.filter(suffix => token.endsWith(suffix))
+    if (matchingSuffixes.length > 0) { 
+      const index = token.indexOf(matchingSuffixes[0])
+      const newToken = token.substring(0, index)
+      return newToken
+    }
+    return token
   }
 
   // Step 5 & 6: Compute TF-IDF weighted vector for a sentence including parameter count
-  vectorize(sentence: string): (number | undefined)[] {
-    // Tokenize sentence (Steps 1-3)
-    // Extract parameter count (Step 4)
-    // Use TF-IDF formula (Step 5)
-    // Return final vector: [ ...weights, paramCount ] (Step 5 & 6)
+  vectorize(tokenizedSentence: string[]): (number | undefined)[] {
+
   }
 
   // Loops through all words from target sentences and stores them in set.
@@ -113,18 +170,9 @@ class commandBot {
 
   }
 
-  // Get current vocabulary as array for vectorization
-  getVocabWords(): Set<string> {
-    
-  }
-
   // Used to find the closest math to the target sentence, can return null if not close enough
   cosineSimilarity(vectorA: number[], vectorB: number[]): number | null {
     
   }
 
-  // Step 8: Find the closest target sentence match to the input sentence
-  findClosestMatch(inputSentence: string): { sentence: string; similarity: number } {
-    
-  }
 }

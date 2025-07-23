dae56cedef69e3c75e5dce59d2ad4fc891dc6dbf
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-23 11:04:57 -0700

    inital call for student ranking, function for category urgency score, outline for each category urgency equation

--- client/src/utils/notificationsRanking.ts
+++ client/src/utils/notificationsRanking.ts
@@ -1,45 +1,49 @@
-const decayEquation = (
+import { NotificationType } from "./NotificationType"
 
-) => {
-  
-}
-/**
- * This function takes in all the notifications fetched and counts the number of 
- * unread  notifications and how many hours they have been unread for each of the 
- * different student categories.
- * 
- * @param notifications - All the notifications fetched.
- * @returns A map with all the count data and the notification type as the key.
- */
-const sortStudentWidgets = (
-  notifications: any
-) => {
-  
-}
-
-
-/**
- * This function takes in all the data saved into the map and sorts the
- * widgets, it then returns the widget name and type.
- * @param unreadData - The data stored in the map from all the fetched
- * notifications
- * @param widgets - The types of widgets being use, student or professor.
- * @returns The widget name and type in an array.
- */
-const sortWidgetsByScore = (
-  
-) => {
-
-}
-
-/**
- * This function takes in the notifications to be sorted for the student and runs 
- * functions to get the notification data to sort and then sorts the notification 
- * categories.
- * 
- * @param notifications - The notifications that decide the category sorting.
- * @returns The widget name and type in a sorted list.
- */
 const fetchedOrderStudentWidgets = (notifications: any) => {
-  
+  const studentCategoryMap = new Map<NotificationType, [data: any, read: boolean]>([]);
+  for (const notification of notifications) {
+    if (studentCategoryMap.has(notification.type)) {
+      studentCategoryMap.get(notification.type)!.push({ data: notification.data, read: notification.read })
+    } else {
+      studentCategoryMap.set(notification.type, { data: notification.data, read: notification.read })
+    }
+  }
+
+  for (const [category, notificationList] of studentCategoryMap) {
+    notificationList.forEach(notification => {
+      calculateNotificationUrgencyScore(category, notification.data)
+    })
+  }
+}
+
+const calculateNotificationUrgencyScore = (category: NotificationType, data: any): number => {
+  switch (category) {
+    case NotificationType.AnnouncementPosted:
+      return decayEquationAnnouncement(data)
+    case NotificationType.FileSystemItemCreated:
+      return decayEquationFileSystem(data)
+    case NotificationType.AssignmentPosted:
+      return assignmentUrgencyEquation(data)
+    case NotificationType.StudentSubmission:
+      return submissionUrgencyEquation(data)
+    default:
+      return 0
+  }
+}
+
+const decayEquationAnnouncement = (data: NotificationData): number => {
+
+}
+
+const decayEquationFileSystem = (data: NotificationData): number => {
+
+}
+
+const assignmentUrgencyEquation = (data: NotificationData): number => {
+
+}
+
+const submissionUrgencyEquation = (data: NotificationData): number => {
+
 }

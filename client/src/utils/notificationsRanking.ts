c3147aa3bb45d669d651c2567cc028be5862489f
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-23 12:00:25 -0700

    implemented assignment and submmission equations, fixed decay equations

--- client/src/utils/notificationsRanking.ts
+++ client/src/utils/notificationsRanking.ts
@@ -1,9 +1,11 @@
 import { NotificationType } from "./NotificationType"
 
 const U_MAX = 1
-const ANNOUNCEMENT_DECAY = 0.01
-const FILE_SYSTEM_DECAY = 0.05
+const ANNOUNCEMENT_DECAY = -0.01
+const FILE_SYSTEM_DECAY = -0.1
+const ASSIGNMENT_SUBMISSION_DECAY = -0.05
 const HOUR_DIVISER = 3600000
+const LATE_WEIGHT = 1
 
 const fetchedOrderStudentWidgets = (notifications: any) => {
   const studentCategoryMap = new Map<NotificationType, any[]>([])
@@ -57,7 +59,8 @@ const decayEquationAnnouncement = (notification: any): number => {
   const timeNow = new Date()
   const notificationCreation = new Date(notification.createdAt)
   const hoursSinceAnnouncement = Math.round(timeNow.getTime() - notificationCreation.getTime() / HOUR_DIVISER)
-  return U_MAX * (Math.exp(ANNOUNCEMENT_DECAY * hoursSinceAnnouncement))
+  const score = U_MAX * (Math.exp(ANNOUNCEMENT_DECAY * hoursSinceAnnouncement))
+  return score <= 0.1 ? 0.01 : score
 }
 
 /**
@@ -70,13 +73,35 @@ const decayEquationFileSystem = (notification: any): number => {
   const timeNow = new Date()
   const notificationCreation = new Date(notification.createdAt)
   const hoursSinceAnnouncement = Math.round(timeNow.getTime() - notificationCreation.getTime() / HOUR_DIVISER)
-  return U_MAX * (Math.exp(FILE_SYSTEM_DECAY * hoursSinceAnnouncement))
+  const score = U_MAX * (Math.exp(FILE_SYSTEM_DECAY * hoursSinceAnnouncement))
+  return score <= 0.1 ? 0.01 : score
 }
 
+/**
+ * 
+ * @param notification 
+ * @returns 
+ */
 const assignmentUrgencyEquation = (notification: any): number => {
-
+  const timeNow = new Date().getTime()
+  const dueDate = new Date(notification.data.dueDate).getTime()
+  const hoursUntilDue = (dueDate - timeNow) / HOUR_DIVISER
+  if (hoursUntilDue < 0) { return U_MAX / (Math.exp(0)) } 
+  const score = U_MAX / (Math.exp(ASSIGNMENT_SUBMISSION_DECAY * (hoursUntilDue)))
+  return score
 }
 
+/**
+ * This function uses the urgency equation to calculate the urgency score of the file system update,
+ * adds a late penalty if it is past the dueDate.
+ * @param notification 
+ * @returns 
+ */
 const submissionUrgencyEquation = (notification: any): number => {
-
+  const timeNow = new Date().getTime()
+  const dueDate = new Date(notification.data.dueDate).getTime()
+  const hoursUntilDue = (dueDate - timeNow) / HOUR_DIVISER
+  if (hoursUntilDue > 0) { return U_MAX / (Math.exp(ASSIGNMENT_SUBMISSION_DECAY * hoursUntilDue)) } 
+  const lateScore = (U_MAX / (Math.exp(ASSIGNMENT_SUBMISSION_DECAY * hoursUntilDue))) + (LATE_WEIGHT * (1 - hoursUntilDue))
+  return lateScore
 }

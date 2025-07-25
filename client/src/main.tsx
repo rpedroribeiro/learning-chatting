cd44308d9f224fb5166690d0bf87652aa1c25686
Pedro Rabadan Ribeiro <prabadanribeiro@gmail.com>
2025-07-24 23:08:56 -0700

    changed target sentences for clarity

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { AuthProvider } from './context/AuthContext.tsx'
+import '@testing-library/jest-dom';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
